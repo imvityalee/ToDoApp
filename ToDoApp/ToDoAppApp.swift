19c342cbc1d7e0c05f03cc3b7bb3873765803fff
Viktor Li <lee.vitya28@gmail.com>
2022-01-24 17:18:26 +0600

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Victor Lee on 24/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
